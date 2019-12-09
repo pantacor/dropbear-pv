89ca0c113df00fdd148a89f9672fd9684b546adc
Alexander Sack <asac@pantacor.com>
2019-12-09 22:37:18 +0100

    change name to dropbear-pv and use zlib as dependency

--- atom.mk
+++ atom.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := fallbear
-LOCAL_DESCRIPTION := fallbear
+LOCAL_MODULE := dropbear-pv
+LOCAL_DESCRIPTION := dropbear-pv
 
-LOCAL_AUTOTOOLS_VERSION := 2.0.4
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS := --enable-static
+LOCAL_LIBRARIES += zlib
 
 LOCAL_CLEAN_DIRS := $(call local-get-build-dir)/usr/share/ $(call local-get-build-dir)/usr/include/
 
